92da8c025a2bb6cea8880d87bdb58ce3edc52374
데스크탑 <z_gahu@naver.com>
2024-04-22 23:34:05 +0900

    회원기능만들기 passport, 로그인기능

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+app.use(passport.initialize());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false, // 유저가 서버로 요청할 때마다 세션갱신할지 여부
+    saveUninitialized: false, // 로그인을 안해도 세션을 만들지 여부
+  })
+);
+app.use(passport.session());
+
 const { MongoClient, ObjectId } = require("mongodb");
 
 let db;
@@ -173,4 +188,44 @@ app.get("/list/next/:id", async (요청, 응답) => {
     .toArray();
   // 응답.render("list.ejs");
   응답.render("list.ejs", { 글목록: result });
+});
+
+passport.use(
+  new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
+    // 제출한 아이디/비번 검사하는 코드
+    let result = await db
+      .collection("user")
+      .findOne({ username: 입력한아이디 });
+    if (!result) {
+      return cb(null, false, { message: "아이디가 디비에 없음" }); // 회원인증 실패시엔 false를 넣어줘야함
+    }
+    if (result.password == 입력한비번) {
+      return cb(null, result);
+    } else {
+      return cb(null, false, { message: "비번불일치" });
+    }
+  })
+);
+
+// 아이디/비번 외에 다른것도 제출받아서 검증가능 passReqToCallback 옵션
+// 실행하고 싶으면 passport.authenticate('local')() 사용
+
+app.post("/login", async (요청, 응답, next) => {
+  // 제출한 아이디 비번이 디비에 있는지 확인하고 있으면 세션만들어줌
+  passport.authenticate("local", (error, user, info) => {
+    if (error) return 응답.status(500).json(error);
+    if (!user) return 응답.status(401).json(info.message); // 위에 적은 메세지
+    요청.logIn(user, (err) => {
+      if (err) return next(err);
+      응답.redirect("/"); //로그인 완료시 실행할 코드
+    }); // 세션 만들기시작
+  })(요청, 응답, next);
+  // - 콜백함수의 첫째 파라미터는 뭔가 에러시 뭔가 들어옴
+  // - 둘째 파라미터는 아이디/비번 검증 완료된 유저정보가 들어옴
+  // - 셋째는 아이디/비번 검증 실패시 에러메세지가 들어옴
+  응답.render("login.ejs");
+});
+
+app.get("/login", async (요청, 응답) => {
+  응답.render("login.ejs");
 });
