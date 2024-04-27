213bd801d219936e9bffd2731768bf01bd30b827
데스크탑 <z_gahu@naver.com>
2024-04-28 00:29:01 +0900

    파일업로드 multer 라이브러리 셋팅

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const MongoStore = require("connect-mongo");
+const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const s3 = new S3Client({
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SCERET_ACCESS_KEY,
+  },
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET,
+    key: function (요청, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
 
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
