055b0d838d170ce12fa027afd5028e234d4145ea
데스크탑 <z_gahu@naver.com>
2024-04-27 20:23:45 +0900

    미들웨어 테스트

--- server.js
+++ server.js
@@ -48,14 +48,15 @@ app.listen(process.env.PORT, () => {
   console.log("http://localhost:8080 에서 서버 실행중");
 });
 
-function 함수(요청, 응답) {
+function checkLogin(요청, 응답, next) {
   if (!요청.user) {
     응답.send("로그인하세요");
   }
+  next(); // 미들웨어 코드 실행 끝났으니 다음으로 이동해주세요
 }
 
-app.get("/", (요청, 응답) => {
-  함수(요청, 응답);
+app.get("/", checkLogin, (요청, 응답) => {
+  // 함수(요청, 응답);
   응답.sendFile(__dirname + "/index.html");
 });
 
